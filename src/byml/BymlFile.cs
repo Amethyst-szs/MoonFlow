819afa728e6ce4274a88f178cfd173a41cb950e9
Amethyst-szs <Amethyst-UwU@protonmail.com>
2024-11-03 21:17:02 -0800

    Cleaned up BymlFile impl

--- src/byml/BymlFile.cs
+++ src/byml/BymlFile.cs
@@ -5,15 +5,8 @@ using System.Linq;
 
 namespace Nindot.Byml;
 
-public class BymlFile : Dictionary<string, object>
+public class BymlFile(Dictionary<string, object> dict) : Dictionary<string, object>(dict)
 {
-    // ====================================================== //
-    // ============== Init and Saving Functions ============= //
-    // ====================================================== //
-
-    public BymlFile(Dictionary<string, object> dict) : base(dict)
-    {
-    }
     public static BymlFile FromFilePath(string path)
     {
         if (!BymlFileAccess.ParseFile(out BymlFile file, path))
@@ -28,33 +21,10 @@ public class BymlFile : Dictionary<string, object>
 
         return file;
     }
-    public void WriteFile(MemoryStream stream)
-    {
-        BymlFileAccess.WriteFile(stream, this);
-    }
+    public void WriteFile(MemoryStream stream) { BymlFileAccess.WriteFile(stream, this); }
 
-
-    public Type GetType(string key)
-    {
-        return this[key].GetType();
-    }
-
-    public Type GetType(int index)
-    {
-        return Values.ElementAt(index).GetType();
-    }
-
-    public bool TryGetType(out Type type, string key)
-    {
-        type = null;
-        if (ContainsKey(key))
-        {
-            type = GetType(key);
-            return true;
-        }
-
-        return false;
-    }
+    public Type GetType(string key) { return this[key].GetType(); }
+    public Type GetType(int index) { return Values.ElementAt(index).GetType(); }
 
     public bool TryGetValue<T>(out T value, string key)
     {
@@ -63,13 +33,9 @@ public class BymlFile : Dictionary<string, object>
             return false;
 
         object obj = this[key];
-        if (!obj.GetType().Equals(value))
-            return false;
-
         value = (T)obj;
         return true;
     }
-
     public bool TryGetValue<T>(out T value, int index)
     {
         value = default;
@@ -79,38 +45,6 @@ public class BymlFile : Dictionary<string, object>
             return false;
 
         value = (T)obj;
-        return true;
-    }
-
-    public bool TryGetIter(out BymlFile value, string key)
-    {
-        value = default;
-        if (!ContainsKey(key))
-            return false;
-
-        object obj = this[key];
-        if (obj.GetType() != typeof(Dictionary<object, object>))
-            return false;
-
-        Dictionary<object, object> dict = (Dictionary<object, object>)obj;
-        Dictionary<string, object> dictS = dict.ToDictionary(k => k.Key.ToString(), k => k.Value);
-        value = new BymlFile(dictS);
-
-        return true;
-    }
-
-    public bool TryGetIter(out BymlFile value, int index)
-    {
-        value = default;
-
-        object obj = Values.ElementAt(index);
-        if (obj.GetType() != typeof(Dictionary<object, object>))
-            return false;
-
-        Dictionary<object, object> dict = (Dictionary<object, object>)obj;
-        Dictionary<string, object> dictS = dict.ToDictionary(k => k.Key.ToString(), k => k.Value);
-        value = new BymlFile(dictS);
-
         return true;
     }
 }
