5a047e21ad416084de70600d902c4a8f5af6b8ff
Amethyst-szs <Amethyst-UwU@protonmail.com>
2024-11-20 18:33:53 -0800

    Some moonflow cleanup

--- src/MoonFlow/example/msbt/MsbtFileTest.cs
+++ src/MoonFlow/example/msbt/MsbtFileTest.cs
@@ -1,77 +1,79 @@
-// using Godot;
-// using System;
+using Godot;
+using System;
 
-// using Nindot.LMS.Msbt;
-// using Nindot.LMS.Msbt.TagLib.Smo;
-// using Nindot.LMS.Msbp;
-// using Nindot.LMS.Msbt.TagLib;
+using Nindot.LMS.Msbt;
+using Nindot.LMS.Msbt.TagLib.Smo;
+using Nindot.LMS.Msbp;
+using Nindot.LMS.Msbt.TagLib;
 
-// public partial class MsbtFileTest : Control
-// {
-// 	public MsbpFile Project;
-// 	public MsbtFile File;
+public partial class MsbtFileTest : Control
+{
+    public MsbpFile Project;
+    public MsbtFile File;
 
-// 	public override void _Ready()
-// 	{
-// 		// Setup project
-// 		Project = MsbpFile.FromFilePath("res://test/lms/msbp/ProjectData-SMO.msbp");
+    public override void _Ready()
+    {
+        // Setup project
+        Project = MsbpFile.FromBytes(FileAccess.GetFileAsBytes("res://example/msbt/ProjectData.msbp"));
 
-// 		// Setup file
-// 		var factory = new MsbtElementFactoryProjectSmo();
-// 		File = MsbtFile.FromFilePath("res://test/lms/msbt/SmoUnitTesting.msbt", factory);
+        // Setup file
+        var factory = new MsbtElementFactoryProjectSmo();
+        File = MsbtFile.FromBytes(FileAccess.GetFileAsBytes("res://example/msbt/SmoUnitTesting.msbt"), factory);
 
-// 		// Create list of keys in file
-// 		var keyContainer = GetNode<VBoxContainer>("Split/FileKeys/Content");
-// 		foreach (var key in File.Content.Keys)
-// 		{
-// 			var button = new Button();
-// 			button.Text = key;
-// 			button.Pressed += () => OpenEntry(key);
-// 			keyContainer.AddChild(button);
-// 		}
-// 	}
+        // Create list of keys in file
+        var keyContainer = GetNode<VBoxContainer>("Split/FileKeys/Content");
+        foreach (var key in File.GetEntryLabels())
+        {
+            var button = new Button
+            {
+                Text = key
+            };
+            button.Pressed += () => OpenEntry(key);
+            keyContainer.AddChild(button);
+        }
+    }
 
-// 	private void OpenEntry(string entryName)
-// 	{
-// 		// Empty out current content box
-// 		var content = GetNode<VBoxContainer>("Split/Content");
-// 		foreach (var child in content.GetChildren())
-// 			content.RemoveChild(child);
+    private void OpenEntry(string entryName)
+    {
+        // Empty out current content box
+        var content = GetNode<VBoxContainer>("Split/Content");
+        foreach (var child in content.GetChildren())
+            content.RemoveChild(child);
 
-// 		// Get access to requested entry
-// 		File.Content.TryGetValue(entryName, out MsbtEntry entry);
-// 		if (entry == null) return;
+        // Get access to requested entry
+        MsbtEntry entry = File.GetEntry(entryName);
+        if (entry == null) return;
 
-// 		// Build content box
-// 		RichTextLabel label = new()
-// 		{
-// 			FitContent = true,
-// 			BbcodeEnabled = true,
-// 		};
+        // Build content box
+        RichTextLabel label = new()
+        {
+            FitContent = true,
+            BbcodeEnabled = true,
+        };
 
-// 		foreach (var element in entry.Elements)
-// 		{
-// 			if (element.IsTag())
-// 			{
-// 				var elementT = (MsbtTagElement)element;
+        foreach (var element in entry.Elements)
+        {
+            if (element.IsTag())
+            {
+                var elementT = (MsbtTagElement)element;
 
-// 				label.PushItalics();
-// 				label.PushColor(new Color(1.0F, 0.7F, 0.7F, 1.0F));
-// 				label.PushFontSize(14);
+                label.PushItalics();
+                label.PushColor(new Color(1.0F, 0.7F, 0.7F, 1.0F));
+                label.PushFontSize(14);
 
-// 				string group = Project.TagGroup_Get(elementT.GetGroupName()).Name;
-// 				string name = elementT.GetTagNameInProject(Project);
+                string group = Project.TagGroup_Get(elementT.GetGroupName()).Name;
+                string name = elementT.GetTagNameInProject(Project);
 
-// 				label.AppendText(group + " - " + name);
+                label.AppendText(group + " - " + name);
 
-// 				label.PopAll();
-// 				continue;
-// 			}
+                label.PopAll();
+                continue;
+            }
 
-// 			label.AppendText(element.GetText());
-// 			continue;
-// 		}
+            label.AppendText(element.GetText());
+            continue;
+        }
 
-// 		content.AddChild(label);
-// 	}
-// }
+        content.AddChild(label);
+    }
+}
