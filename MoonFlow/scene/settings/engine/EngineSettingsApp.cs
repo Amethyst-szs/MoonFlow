c8b2f3dc3df9266f22f81550537559f9eb5f471d
Amethyst-szs <Amethyst-UwU@protonmail.com>
2024-12-28 18:27:39 -0800

    Add vsync toggle

--- MoonFlow/scene/settings/engine/EngineSettingsApp.cs
+++ MoonFlow/scene/settings/engine/EngineSettingsApp.cs
@@ -6,9 +6,13 @@ namespace MoonFlow.Scene.Settings;
 [ScenePath("res://scene/settings/engine/engine_settings_app.tscn")]
 public partial class EngineSettingsApp : AppScene
 {
-    private void OnSaveAndExit()
+    public override void _ExitTree()
     {
         EngineSettings.Save();
+    }
+
+    private void OnSaveAndExit()
+    {
         AppClose(true);
     }
 }
