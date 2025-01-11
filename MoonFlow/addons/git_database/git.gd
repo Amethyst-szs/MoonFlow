87da7f585c8849696d827585eec14bb8081dbc5c
Amethyst-szs <Amethyst-UwU@protonmail.com>
2025-01-11 11:48:52 -0800

    Tweak compiler warnings for all csproj

--- MoonFlow/addons/git_database/git.gd
+++ MoonFlow/addons/git_database/git.gd
@@ -3,17 +3,17 @@
 extends Object
 class_name GitInfo
 
-const version_name: String = "b354 - dev (2fbbc81d)"
+const version_name: String = "b373 - dev (0d54fd01)"
 
 const branch: String = "dev"
 const author: String = "Amethyst-szs"
 
-const commit_subject: String = "Add 10 minute timeout to tests"
+const commit_subject: String = "Move github_asset to .github/asset"
 const commit_author: String = "Amethyst-szs"
-const commit_hash: String = "2fbbc81d9c3a36165e4e2036d7a09643130d89fd"
-const commit_hash_short: String = "2fbbc81d"
+const commit_hash: String = "0d54fd01ba029ce91f0f12af8e82b536c5964ea2"
+const commit_hash_short: String = "0d54fd01"
 
-const commit_count: int = 354
+const commit_count: int = 373
 const commit_count_stable: int = 353
-const commit_ahead: int = 1
-const commit_time_unix: int = 1736536286
+const commit_ahead: int = 20
+const commit_time_unix: int = 1736624340
