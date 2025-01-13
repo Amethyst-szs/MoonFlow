53fadd28edade8557f8e40fd6260cc3e799159de
Amethyst-szs <Amethyst-UwU@protonmail.com>
2025-01-12 19:00:27 -0800

    Fix CS wrapper for git_database

--- MoonFlow/addons/git_database/Wrapper.cs
+++ MoonFlow/addons/git_database/Wrapper.cs
@@ -2,33 +2,22 @@ using Godot;
 
 namespace MoonFlow.Addons;
 
-#pragma warning disable IDE0051
-
 public static class GitInfo
 {
-    private static GodotObject Instance;
+    private static readonly GDScript Data = GD.Load<GDScript>("res://addons/git_database/git.gd");
 
-    public static string GitVersionName() { return Instance.Get("version_name").AsString(); }
+    public static string GitVersionName() { return Data.Get("version_name").AsString(); }
 
-    public static string GitBranch() { return Instance.Get("branch").AsString(); }
-    public static string GitAuthor() { return Instance.Get("author").AsString(); }
+    public static string GitBranch() { return Data.Get("branch").AsString(); }
+    public static string GitAuthor() { return Data.Get("author").AsString(); }
 
-    public static string GitCommitSubject() { return Instance.Get("commit_subject").AsString(); }
-    public static string GitCommitAuthor() { return Instance.Get("commit_author").AsString(); }
-    public static string GitCommitHash() { return Instance.Get("commit_hash").AsString(); }
-    public static string GitCommitHashShort() { return Instance.Get("commit_hash_short").AsString(); }
+    public static string GitCommitSubject() { return Data.Get("commit_subject").AsString(); }
+    public static string GitCommitAuthor() { return Data.Get("commit_author").AsString(); }
+    public static string GitCommitHash() { return Data.Get("commit_hash").AsString(); }
+    public static string GitCommitHashShort() { return Data.Get("commit_hash_short").AsString(); }
 
-    public static string GitCommitCount() { return Instance.Get("commit_count").AsString(); }
-    public static string GitCommitCountStable() { return Instance.Get("commit_count_stable").AsString(); }
-    public static string GitCommitAhead() { return Instance.Get("commit_ahead").AsString(); }
-    public static string GitCommitUnixTime() { return Instance.Get("commit_time_unix").AsString(); }
-
-    #region Initilization
-
-    [StartupTask]
-    private static void SetupSingletonInstance() { Instance = Engine.GetSingleton("GitInfoInst"); }
-
-    #endregion
+    public static string GitCommitCount() { return Data.Get("commit_count").AsString(); }
+    public static string GitCommitCountStable() { return Data.Get("commit_count_stable").AsString(); }
+    public static string GitCommitAhead() { return Data.Get("commit_ahead").AsString(); }
+    public static string GitCommitUnixTime() { return Data.Get("commit_time_unix").AsString(); }
 }
-
-#pragma warning restore IDE0051
--- MoonFlow/addons/git_database/plugin/instance_register.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-func _enter_tree() -> void:
-	Engine.register_singleton("GitInfoInst", GitInfo.new())
-
-func _exit_tree() -> void:
-	Engine.unregister_singleton("GitInfoInst")
