0bfd0b4d198c38f6067088f228233b52e2c2c4e7
Amethyst-szs <Amethyst-UwU@protonmail.com>
2025-01-12 22:52:39 -0800

    Fix Label_TranslateDefaultWarning autowrap

--- MoonFlow/addons/git_database/git.gd
+++ MoonFlow/addons/git_database/git.gd
@@ -3,17 +3,17 @@
 extends Object
 class_name GitInfo
 
-const version_name: String = "b373 - dev (0d54fd01)"
+const version_name: String = "b392 - stable (3327537e)"
 
-const branch: String = "dev"
+const branch: String = "stable"
 const author: String = "Amethyst-szs"
 
-const commit_subject: String = "Move github_asset to .github/asset"
+const commit_subject: String = "Fix autoload error & Nindot.Tests race condition"
 const commit_author: String = "Amethyst-szs"
-const commit_hash: String = "0d54fd01ba029ce91f0f12af8e82b536c5964ea2"
-const commit_hash_short: String = "0d54fd01"
+const commit_hash: String = "3327537e925250803b7be092b176c8ba06705d73"
+const commit_hash_short: String = "3327537e"
 
-const commit_count: int = 373
-const commit_count_stable: int = 353
-const commit_ahead: int = 20
-const commit_time_unix: int = 1736624340
+const commit_count: int = 392
+const commit_count_stable: int = 392
+const commit_ahead: int = 0
+const commit_time_unix: int = 1736750612
