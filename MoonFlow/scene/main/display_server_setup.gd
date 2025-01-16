31b1b6fce59060c7aff2572c8f7dc11a46f4c510
Amethyst-szs <Amethyst-UwU@protonmail.com>
2025-01-15 19:45:54 -0800

    Cleanup to display server setup

--- MoonFlow/scene/main/display_server_setup.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-func _ready() -> void:
-	DisplayServer.window_set_min_size(Vector2i(799, 527))
-	DisplayServer.window_set_title("MoonFlow")
---
+++ MoonFlow/scene/settings/engine/global/display_server_utility.gd
@@ -0,0 +1,28 @@
+extends Node
+
+func _ready() -> void:
+	set_minimum_size()
+	set_default_window_title()
+
+static func reset_display_server() -> void:
+	# Reset display server mode
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	
+	# Reset window position and size
+	var win: Window = Engine.get_main_loop().current_scene.get_window()
+	
+	var x: int = ProjectSettings.get_setting("display/window/size/viewport_width", 1152)
+	var y: int = ProjectSettings.get_setting("display/window/size/viewport_height", 648)
+	win.size = Vector2i(x, y)
+	
+	win.move_to_center()
+	win.grab_focus()
+	
+	set_minimum_size()
+
+static func set_minimum_size() -> void:
+	DisplayServer.window_set_min_size(Vector2i(799, 527))
+
+static func set_default_window_title() -> void:
+	var n: String = ProjectSettings.get_setting("application/config/name", "MoonFlow")
+	DisplayServer.window_set_title(n)
