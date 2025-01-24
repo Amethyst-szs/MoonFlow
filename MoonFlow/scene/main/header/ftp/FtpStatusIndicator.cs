74be85053cf233fdb82271a795ea6488e1804ead
Amethyst-szs <Amethyst-UwU@protonmail.com>
2025-01-23 16:04:50 -0800

    Add upload queue & progress for FTP uploads

--- MoonFlow/scene/main/header/ftp/FtpStatusIndicator.cs
+++ MoonFlow/scene/main/header/ftp/FtpStatusIndicator.cs
@@ -1,13 +1,25 @@
 using Godot;
-using MoonFlow.Project;
 using System;
+
+using MoonFlow.Project;
+
+using FluentFTP;
+using MoonFlow.Project.FTP;
 
 namespace MoonFlow.Scene.Main;
 
 public partial class FtpStatusIndicator : HBoxContainer, IProjectFtpStatusIndicator
 {
-	private const string AnimPlayerName = "Animation";
-	private const string TooltipContext = "FTP_STATUS_INDICATOR_TOOLTIP";
+	[Export, ExportGroup("Internal References")]
+	private AnimationPlayer Animation;
+	[Export]
+	private ProgressBar ProgressBar;
+	[Export]
+	private Label ProgressLabel;
+
+    #region Display
+
+    private const string TooltipContext = "FTP_STATUS_INDICATOR_TOOLTIP";
 
 	public void SetStatusActive() => UpdateStatus("active", CursorShape.Busy);
 	public void SetStatusConnecting() => UpdateStatus("try_connect", CursorShape.Busy);
@@ -19,9 +31,50 @@ public partial class FtpStatusIndicator : HBoxContainer, IProjectFtpStatusIndica
 
 	private void UpdateStatus(string anim, CursorShape shape)
 	{
-		GetNode<AnimationPlayer>(AnimPlayerName).Play(anim);
+		if (Animation.CurrentAnimation != anim)
+			Animation.Play(anim);
+		
 		TooltipText = Tr(anim, TooltipContext);
-
 		MouseDefaultCursorShape = shape;
 	}
+
+	#endregion
+
+	#region Progress Updates
+
+	public void OnProgressUpdate(FtpProgress data)
+	{
+		bool isInProgress = data.Progress != 100F;
+		ProgressBar.SetDeferred(PropertyName.Visible, isInProgress);
+
+		// A value of -1 on progress indicates an indeterminate transfer
+		ProgressBar.SetDeferred(ProgressBar.PropertyName.Indeterminate, data.Progress == -1F);
+
+		// Update progress
+		ProgressBar.SetDeferred(ProgressBar.PropertyName.Value, data.Progress);
+		ProgressLabel.SetDeferred(Label.PropertyName.Text, data.RemotePath);
+
+		// Generate tooltip
+		var tooltip = string.Format("ETA: {0:c}\n{1}", data.ETA, data.TransferSpeedToString());
+		ProgressBar.SetDeferred(PropertyName.TooltipText, tooltip);
+	}
+
+	#endregion
+
+	#region Signals
+
+	[Signal]
+    public delegate void FtpServerConnectedEventHandler();
+	[Signal]
+    public delegate void FtpServerDisconnectedEventHandler();
+
+	public void AttachEventConnected(Action func) => Connect(SignalName.FtpServerConnected, Callable.From(func));
+    public void AttachEventDisconnected(Action func) => Connect(SignalName.FtpServerDisconnected, Callable.From(func));
+    public void DetachEventConnected(Action func) => Disconnect(SignalName.FtpServerConnected, Callable.From(func));
+    public void DetachEventDisconnected(Action func) => Disconnect(SignalName.FtpServerDisconnected, Callable.From(func));
+
+	public void EmitEventConnected() => EmitSignal(SignalName.FtpServerConnected);
+	public void EmitEventDisconnected() => EmitSignal(SignalName.FtpServerDisconnected);
+
+	#endregion
 }
