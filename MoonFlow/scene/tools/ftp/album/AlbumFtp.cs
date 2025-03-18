8e0d6ef92185fa3372b98edb39855371721d68ea
Amethyst-szs <Amethyst-UwU@protonmail.com>
2025-03-17 18:06:05 -0700

    Progress on AlbumFtpClient backend

--- MoonFlow/scene/tools/ftp/album/AlbumFtp.cs
+++ MoonFlow/scene/tools/ftp/album/AlbumFtp.cs
@@ -7,22 +7,26 @@ namespace MoonFlow.Scene.Tools;
 [SceneUid("uid://dlqa3a1xwd8sm"), Icon("res://asset/nindot/lms/icon/DeviceFont_Album.png")]
 public partial class AlbumFtp : AppScene
 {
+    private AlbumFtpClient Client = new();
+
     [Export, ExportGroup("Internal References")]
     private Control Container;
 
-    public override async void _Ready()
+    public override void _Ready()
     {
-        byte[] file = await ProjectFtpClient.Client.DownloadBytes("/Nintendo/Album/2025/03/12/2025031210535800-8AEDFF741E2D23FBED39474178692DAF.jpg", 0);
+        Client.InitAlbumClient();
         
-        var image = new Image();
-        image.LoadJpgFromBuffer(file);
+        // byte[] file = await ProjectFtpClient.Client.DownloadBytes("/Nintendo/Album/2025/03/12/2025031210535800-8AEDFF741E2D23FBED39474178692DAF.jpg", 0);
+        
+        // var image = new Image();
+        // image.LoadJpgFromBuffer(file);
 
-        var tex = ImageTexture.CreateFromImage(image);
-        var panel = new TextureRect
-        {
-            Texture = tex
-        };
+        // var tex = ImageTexture.CreateFromImage(image);
+        // var panel = new TextureRect
+        // {
+        //     Texture = tex
+        // };
 
-        Container.AddChild(panel);
+        // Container.AddChild(panel);
     }
 }
