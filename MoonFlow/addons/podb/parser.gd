e86ca0c5bb8eb148bb0a950aa23f1f54c76836b5
Amethyst-szs <Amethyst-UwU@protonmail.com>
2025-07-09 12:07:03 -0700

    Complete CheckpointFlagInfo generator

--- MoonFlow/addons/podb/parser.gd
+++ MoonFlow/addons/podb/parser.gd
@@ -19,6 +19,10 @@ func _parse_file(path: String) -> Array[PackedStringArray]:
 func _parse_type_translation_bank(res: TranslationBank, ret: Array[PackedStringArray]) -> void:
 	for str in res.keys:
 		ret.append(PackedStringArray([str, res.context, ""]))
+		
+		if res.create_multiple_contexts:
+			for ctx in res.additional_contexts:
+				ret.append(PackedStringArray([str, ctx, ""]))
 
 func _parse_type_contributor_list(res: ContributorList, ret: Array[PackedStringArray]) -> void:
 	ret.append(PackedStringArray([res.list_name]))
--- MoonFlow/addons/podb/translation_db.gd
+++ MoonFlow/addons/podb/translation_db.gd
@@ -3,3 +3,7 @@ extends Resource
 
 @export var context: String = ""
 @export var keys: Array[String] = []
+
+@export_group("Additional Options")
+@export var create_multiple_contexts: bool = false
+@export var additional_contexts: Array[String] = []
