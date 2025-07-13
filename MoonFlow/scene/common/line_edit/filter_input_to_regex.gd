3bd2587c87d412c195e82af0359bfa7649e51eb5
Amethyst-szs <Amethyst-UwU@protonmail.com>
2025-07-13 13:13:29 -0700

    Fix bug in LineEdit regex validator

--- MoonFlow/scene/common/line_edit/filter_input_to_regex.gd
+++ MoonFlow/scene/common/line_edit/filter_input_to_regex.gd
@@ -15,12 +15,18 @@ func _on_text_changed(new_text: String) -> void:
 	if is_return_if_empty && new_text.is_empty():
 		return
 	
-	var old_caret_position = caret_column
-	var word = ""
+	var old_caret_position := caret_column
 	
+	# Calculate the string filtered by regex
+	var word = ""
 	for valid_character in regex.search_all(new_text):
 		word += valid_character.get_string()
 	
+	# If the new text and the filtered text are the same, return early
+	if new_text == word:
+		text_validated.emit(new_text)
+		return
+	
 	set_text(word)
-	set_deferred("caret_column", old_caret_position)
+	set_deferred("caret_column", old_caret_position - 1)
 	text_validated.emit(word)
