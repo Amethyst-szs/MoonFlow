24ddf669892ba2bfd709f19d0a31e604531f17e3
Amethyst-szs <Amethyst-UwU@protonmail.com>
2025-07-12 21:50:24 -0700

    Update library versions

--- Nindot/Sarc/NindotYaz0.cs
+++ Nindot/Sarc/NindotYaz0.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.IO.Compression;
 
 using AuroraLib.Compression.Algorithms;
 using AuroraLib.Core.IO;
@@ -7,27 +8,29 @@ namespace Nindot;
 
 public static class NindotYaz0
 {
+    private const CompressionLevel CompressionType = CompressionLevel.Fastest;
+
     public static byte[] Compress(byte[] source)
     {
         var result = new MemoryStream();
-        new Yaz0() { LookAhead = false }.Compress(source, result);
+        new Yaz0() { LookAhead = true }.Compress(source, result, CompressionType);
 
         return result.ToArray();
     }
     public static byte[] Compress(Stream source)
     {
         var result = new MemoryStream();
-        new Yaz0() { LookAhead = false }.Compress(source.ToArray(), result);
+        new Yaz0() { LookAhead = true }.Compress(source.ToArray(), result, CompressionType);
 
         return result.ToArray();
     }
     public static void Compress(byte[] source, Stream result)
     {
-        new Yaz0() { LookAhead = false }.Compress(source, result);
+        new Yaz0() { LookAhead = true }.Compress(source, result, CompressionType);
     }
     public static void Compress(Stream source, Stream result)
     {
-        new Yaz0() { LookAhead = false }.Compress(source.ToArray(), result);
+        new Yaz0() { LookAhead = true }.Compress(source.ToArray(), result, CompressionType);
     }
 
     public static byte[] Decompress(byte[] source)
