0413fc4d486d6d37afa2080f35005f9fa3dd32b6
Amethyst-szs <Amethyst-UwU@protonmail.com>
2025-09-16 12:21:02 -0700

    Move CheckpointFlagInfo generator to Project tab

--- MoonFlow/scene/home/tools/project/TabProject.cs
+++ MoonFlow/scene/home/tools/project/TabProject.cs
@@ -1,11 +1,24 @@
 using Godot;
 using System;
 
+using MoonFlow.Async;
+using MoonFlow.Project;
+using MoonFlow.Project.Database;
 using MoonFlow.Scene.EditorMsbt;
 
 namespace MoonFlow.Scene.Home;
 
 public partial class TabProject : HSplitContainer
 {
+    private void OnGenerateDatabaseCheckpointFlag()
+	{
+		AsyncRunner.Run(GenerateDatabaseCheckpointFlag, AsyncDisplay.Type.GenerateCheckpointDb, "dbgen_checkpoint_flag");
+	}
+	private void GenerateDatabaseCheckpointFlag(AsyncDisplay display)
+	{
+		var db = ProjectManager.GetDB();
+		CheckpointFlagDbGenerator.Generate(db, display.UpdateProgress);
+	}
+
     private static void OnOpenMsbpColorEditor() { AppSceneServer.CreateApp<MsbpColorEditor>(); }
 }
